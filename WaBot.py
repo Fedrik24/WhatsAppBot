eef3d12a6581f6fdf60c1a3d97c3e409fc6b1812
Fedrik24 <69137510+Fedrik24@users.noreply.github.com>
2020-12-16 07:00:34 +0700

    Second

--- WaBot.py
+++ WaBot.py
@@ -10,8 +10,47 @@ import sys
 
 
 class BotTime:
-    def Test(self):
-        print("testing")
+    def element_presence(self,by,xpath,time):
+        element_present = EC.presence_of_element_located((By.XPATH, xpath))
+        WebDriverWait(driver, time).until(element_present)
+
+    def is_connected(self):
+        try:
+            # connect to the host -- tells us if the host is actually
+            # reachable
+            socket.create_connection(("www.google.com", 80))
+            return True
+        except :
+            self.is_connected()
+    driver = webdriver.Chrome(executable_path="chromedriver.exe")
+    driver.get("http://web.whatsapp.com")
+    sleep(10) #wait time to scan the code in second
+
+    def send_whatsapp_msg(self,phone_no,text):
+        driver.get("https://web.whatsapp.com/send?phone={}&source=&data=#".format(phone_no))
+        try:
+            driver.switch_to_alert().accept()
+        except Exception as e:
+            pass
+
+        try:
+            element_presence(self,By.XPATH,'//*[@id="main"]/footer/div[1]/div[2]/div/div[2]',30)
+            txt_box=driver.find_element(By.XPATH , '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+            global no_of_message
+            for x in range(no_of_message):
+                txt_box.send_keys(text)
+                txt_box.send_keys("\n")
+
+        except Exception as e:
+            print("invailid phone no :"+str(phone_no))
+    for moblie_no in moblie_no_list:
+        try:
+            self,send_whatsapp_msg(moblie_no,message_text)
+
+        except Exception as e:
+            sleep(10)
+            self.is_connected()
+
 
 class User:
 
@@ -19,20 +58,25 @@ class User:
         print("Enter Single Phone Number Here!")
         user = input("Enter Phone Number : ")
         print(62,int(user))
-        BotTime.Test(self)
+        BotTime()
 
-    def UserMultipleNo(self):
+    def UserMultipleNo(self,**args):
         print("Enter Multiple Phone Number Here!")
         print("Under Control!")
 
+
+######################## USER COMMAND ##########################
     def User_Choice(self):
         print(""" 1.Single Number \n 2.Multiple Number""")
         c = input("Enter Number ")
-        while c not in ['Null']:
+        while c not in ['q','quit']:
             if c == '1':
                 self.UserSingleNo()
-            else:
+            elif c == '2':
                 self.UserMultipleNo()
+            else:
+                print('Bye~')
+                sys.exit()
             break
 
 
